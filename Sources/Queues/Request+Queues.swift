293123e3493fc62b9c661fe24fe70f66b33c06cd
Jorge Costa <jorgeefrazaocosta@gmail.com>
2024-12-27 16:36:11 +0000

    Fix code

--- Sources/Queues/Request+Queues.swift
+++ Sources/Queues/Request+Queues.swift
@@ -4,14 +4,14 @@ import NIOCore
 
 extension Request {
     /// Get the default ``Queue``.
-    public var queue: any Queue {
+    public var queue: any AsyncQueue {
         self.queues(.default)
     }
 
     /// Create or look up an instance of a named ``Queue`` and bind it to this request's event loop.
     ///
     /// - Parameter queue: The queue name
-    public func queues(_ queue: QueueName, logger: Logger? = nil) -> any Queue {
+    public func queues(_ queue: QueueName, logger: Logger? = nil) -> any AsyncQueue {
         self.application.queues.queue(queue, logger: logger ?? self.logger, on: self.eventLoop)
     }
 }
